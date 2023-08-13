33d699d121603c96d5b7a513dbac0aae14f9b130
migsilva89 <migdrum@gmail.com>
2023-08-14 00:02:21 +0100

    add notfound page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes, Navigate } from 'react-router-d
 import Login from './pages/Login.tsx';
 import Dashboard from './pages/Dashboard.tsx';
 import { useAuth } from './contexts/useAuth.ts';
+import NotFound from './pages/NotFound.tsx';
 
 function App() {
   const { isAuthenticated } = useAuth();
@@ -9,6 +10,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path='*' element={<NotFound />} />
         <Route path='/' element={<Login />} />
         <Route path='/dashboard' element={isAuthenticated ? <Dashboard /> : <Navigate to='/' />} />
       </Routes>
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound: React.FC = () => {
+  return (
+    <div className='text-white'>
+      <h1>404 - Page Not Found</h1>
+      <p>This page does not exist.</p>
+    </div>
+  );
+};
+
+export default NotFound;
