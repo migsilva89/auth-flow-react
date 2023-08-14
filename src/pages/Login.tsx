c08aa440856f20d0ec9aa34eab1855ed299e9c48
migsilva89 <migdrum@gmail.com>
2023-08-14 10:58:55 +0100

    add notfound page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -14,20 +14,22 @@ const Login = () => {
   }, [isAuthenticated, navigate]);
 
   return (
-    <main className='bg-gray-900'>
+    <>
       <header className='text-center mt-8'>
         <img src='/logo-blue.png' alt='Your Logo' className='mx-auto w-96' />
       </header>
-      <LoginForm />
-      <div className='text-center'>
-        <p className='text-white text-md'>Use below credentials for testing:</p>
-        <p className='text-gray-300 text-sm mt-2'>
-          Username: kminchelle
-          <br />
-          Password: 0lelplR
-        </p>
-      </div>
-    </main>
+      <main>
+        <LoginForm />
+        <div className='text-center'>
+          <p className='text-white text-md'>Use below credentials for testing:</p>
+          <p className='text-gray-300 text-sm mt-2'>
+            Username: kminchelle
+            <br />
+            Password: 0lelplR
+          </p>
+        </div>
+      </main>
+    </>
   );
 };
 
